df873fe494a7e7d306a47ae1b5fc94510992ebfa
Khairil Rizal <manjacat@gmail.com>
2018-07-20 01:21:34 +0800

    Latest code
    latest code

---
+++ webapp/Models/MonthModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eSPP.Models
+{
+    public class MonthModel
+    {
+        public string Jan { get; set; }
+        public string Feb { get; set; }
+        public string Mac { get; set; }
+        public string Apr { get; set; }
+        public string Mei { get; set; }
+        public string Jun { get; set; }
+        public string Julai { get; set; }
+        public string Ogos { get; set; }
+        public string Sept { get; set; }
+        public string Okt { get; set; }
+        public string Nov { get; set; }
+        public string Dis { get; set; }
+    }
+}
---
+++ webapp/Models/TextFileModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eSPP.Models
+{
+    public class TextFileModel
+    {
+        public string String1 { get; set; }
+        public string String2 { get; set; }
+        public string String3 { get; set; }
+        public string String4 { get; set; }
+        public string String5 { get; set; }
+        public string String6 { get; set; }
+    }
+}
